f3ae4d2d0048757ed0e0ade15b78e83af233d61d
Keila Gonzalez <keilagonzalezlopez15@gmail.com>
2025-07-19 15:07:26 -0300

    Agregue npc nivel medio

--- clase_NPC.py
+++ clase_NPC.py
@@ -14,3 +14,16 @@ class NPC:
     
     def verificar_respuesta(self, respuesta_jugador):
         return respuesta_jugador.strip().lower() == self.respuesta
+    
+    # Clase NPC Nivel Medio  
+class NPCMedio(NPC):
+    def __init__(self, x, y, nombre, acertijo, respuesta):
+        # NPCs medios tienen 15 segundos para responder
+        super().__init__(x, y, nombre, acertijo, respuesta, 15, "Medio")
+        
+    def penalizacion_tiempo(self, tiempo_transcurrido):
+        """Los NPCs medios tienen penalización si tardas mucho"""
+        if tiempo_transcurrido > self.tiempo_limite * 0.7:
+            print("⚠️ Tardaste demasiado, pero aún puedes responder...")
+            return True
+        return False
